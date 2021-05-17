e84a1bba3d924684120c8cc4a8030caf0ea7425f
flavio216 <flavio_ezequiel54@hotmail.com>
2021-05-17 19:18:53 -0300

    Revert "17/05/21 ExamenTacticasoft 1.0"
    This reverts commit 707e364fb015a44633b947cb71a5860a028acd6d.

--- NobisSA/Program.cs
+++ NobisSA/Program.cs
@@ -16,7 +16,7 @@ namespace NobisSA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmTramites());
+            Application.Run(new frmSucursales());
         }
     }
 }
